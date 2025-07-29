ce110491e48a0807a4076a8f067bfa1bfe66d5cc
filip <philip.thomson@student.onslow.school.nz>
2025-07-30 11:07:09 +1200

    deleted play stuff and worked on ui

--- game_manager.gd
+++ game_manager.gd
@@ -1,34 +1,54 @@
 extends Node2D
-
-var counter: float = 0
-var is_held: bool = false
-
-@onready var sprite_2d: Sprite2D = $Sprite2D
-@onready var label: Label = $Sprite2D/Label
-
+# tags that are used to changed the aspects of the font
 var tags: String = ""
+
+# what the text is
 var actual_text: String = ""
 
+# giving the RichTextlabel a meaning so its easier to read and use
+@onready var dialogue: RichTextLabel = $RichTextLabel
+
+# the raw text within the file as it is displayed in the .txt
+var raw_text_file: FileAccess
+
+# an empty array that is strings
+var TextFileArrey: Array[String]
+
+# function that has fileaccess and returns an array of strings
+func file_to_array(raw_file: FileAccess) -> Array[String]:
+	#empty var of strings
+	var cur_line: String = ""
+	
+	# what the final array of string is
+	var final_array: Array[String]
+	
+	# a for loop for raw files as text
+	for letter in raw_file.get_as_text():
+		# checking if its a new line
+		if letter == "\n": 
+			# appends it onto the empty var
+			final_array.append(cur_line)
+			cur_line = ""
+		# if its not a new line it adds the letter it finds onto current line
+		else: cur_line += letter
+	# returns the final array of current line
+	return final_array
+
+# function that occurs the moment an element appears
 func _ready() -> void:
-	print("hello world UwU")
+	# giving raw text file access to the .txt and making it read it
+	raw_text_file = FileAccess.open("res://text files/example.txt", FileAccess.READ)
+	
+	# run the file_to_array on the raw_text_file
+	TextFileArrey = file_to_array(raw_text_file)
 
+# function that works on every frame that is being shown
 func _process(delta: float) -> void:
-	if is_held == false:
-		counter += 1
-	else:
-		counter -= 1
-	
-	label.scale = Vector2(counter/100,counter/100)
-	label.text = str(label.scale)
-	
-	sprite_2d.position = Vector2(counter, counter)
-	
-	tags = "[color=#008000][font_size=" + str(counter/5) + "]"
-	
-	actual_text = str(counter)
-	$RichTextLabel.text = tags + actual_text
-
-func _physics_process(delta: float) -> void:
-	if Input.is_action_just_pressed("reset"): counter = 0
-	if Input.is_action_pressed("reverse"): is_held = true
-	else: is_held = false
+	# changing the style of the font
+	tags = "[color=#000000][font_size=10]"
+	# displaying the text as spesific line
+	actual_text = TextFileArrey[2]
+	# combining the elements and the text together
+	dialogue.text = tags + actual_text
+	# setting its size
+	dialogue.size = Vector2(500, 500)
